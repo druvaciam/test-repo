41ebb8ea962aef6c4552515f24287c6306911e0a
druvaciam <druvaciam@gmail.com>
2015-01-22 12:30:07 +0300

    all reload

--- R/lminfile.R
+++ R/lminfile.R
@@ -1,7 +1,8 @@
-lminfile <- function(xcount = 2, yparams = c("t10_mouth", "t3_mouth"),
+lminfile <- function(xcount = 2, yparams = c("Result"),
                      filename = "t10_mouth.txt") {
-  xparams <- c("KFK", "Kortizol", "LN", "Mochevina", "Testosteron",
-               "Gemaglobin", "Alt", "Ast", "Glukoza", "Leicociti", "Tg")
+  #xparams <- c("KFK", "Kortizol", "LN", "Mochevina", "Testosteron",
+  #             "Gemaglobin", "Alt", "Ast", "Glukoza", "Leicociti", "Tg")
+  xparams <- c("ÄÀÒÀ", "ÀÏ1", "ÀÏ2", "ÏÀÍÎ1", "ÏÀÍÎ2", "ÏÀÍÎ3", "GB1", "GB2", "GB3", "ìî÷åâèíà", "ìî÷åâèíà.âå÷åð", "ÊÔÊ", "ãëþêîçà", "ÀÑÒ", "ÀËÒ", "ãåì.ò", "ãåì.í", "L.N", "TG")
   
   splitedname <- unlist(strsplit(filename, "[.]"))
   outfilename <- paste(splitedname[1], "_out", ".", splitedname[2], sep = "")
@@ -25,11 +26,13 @@ lminfile <- function(xcount = 2, yparams = c("t10_mouth", "t3_mouth"),
             newlm <- lm(formula = df[, yparam] ~ df[, 2])
           }
           if (xcount == 2) {
-            newlm <- lm(formula = df[, yparam] ~ sqrt(df[, 2]) + sqrt(df[, 3])
-                        + df[, 2] + df[, 3])
+            newlm <- lm(formula = df[, yparam] ~ df[, 2] + df[, 3])
+            #newlm <- lm(formula = df[, yparam] ~ sqrt(df[, 2]) + sqrt(df[, 3]))
+                        #+ df[, 2] + df[, 3])
           }
           else if (xcount == 3) {
-            newlm <- lm(formula = df[, yparam] ~ sqrt(df[, 2]) + sqrt(df[, 3]) + sqrt(df[, 4]))
+            newlm <- lm(formula = df[, yparam] ~ df[, 2] + df[, 3] + df[, 4])
+            #newlm <- lm(formula = df[, yparam] ~ sqrt(df[, 2]) + sqrt(df[, 3]) + sqrt(df[, 4]))
                         #+ df[, 2] + df[, 3] + df[, 4])
           }
           else if (xcount == 4) {
@@ -38,10 +41,10 @@ lminfile <- function(xcount = 2, yparams = c("t10_mouth", "t3_mouth"),
           outstr <- paste(s$sex[[1]], ":", yparam, "~")
           #outstr <- paste("m + f", ":", yparam, "~")
           eq <- paste("(", as.character(newlm$coef[1]), ")", "+",
-                      "(", as.character(newlm$coef[2]), ")", "*", "sqrt(x)", "+",
-                      "(", as.character(newlm$coef[3]), ")", "*", "sqrt(y)", "+",
-                      "(", as.character(newlm$coef[4]), ")", "*", "x", "+",
-                      "(", as.character(newlm$coef[5]), ")", "*", "y",
+                      #"(", as.character(newlm$coef[2]), ")", "*", "sqrt(x)", "+",
+                      #"(", as.character(newlm$coef[3]), ")", "*", "sqrt(y)", "+",
+                      "(", as.character(newlm$coef[2]), ")", "*", "x1", "+",
+                      "(", as.character(newlm$coef[3]), ")", "*", "x2",
                       sep = "")
           rsqr <- paste("R-squared:", as.character(summary(newlm)$r.squared))
           for (x in xparams[xindexes]) {
